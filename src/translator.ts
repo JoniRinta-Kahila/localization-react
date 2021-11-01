c959f61a2aa454f1eba3da6c1f9cc060d00bac88
JoniRinta-Kahila <joni.rintakahila@opp.eduvantaa.fi>
2021-11-01 14:24:07 +0200

    add translation paths

--- src/translator.ts
+++ src/translator.ts
@@ -1,6 +1,8 @@
 export interface ITranslation {
   [countryCode: string]: {
-    [key: string]: string,
+    [path: string]: {
+      [key: string]: string,
+    },
   },
 };
 
@@ -8,9 +10,11 @@ export default class Translations {
 
   strings: ITranslation;
   language: string;
+  path: string;
 
-  constructor(strings: ITranslation, language: string) {
+  constructor(strings: ITranslation, path: string, language: string) {
     this.strings =  strings;
+    this.path = path;
     this.language = language;
   };
 
@@ -18,8 +22,11 @@ export default class Translations {
     
     if (!this.strings[this.language])
       throw new Error(`an error occurred while getting translation for [${string}] in [${this.language}], Language ${this.language} does not exists.`);
+    
+    if (!this.strings[this.language][this.path])
+      throw new Error(`an error occurred while getting translation for [${string}] in [${this.path}] in [${this.language}], Path ${this.path} does not exists.`);
 
-    const translation = this.strings[this.language][string];
+    const translation = this.strings[this.language][this.path][string];
 
     if (!translation)
       throw new Error(`an error occurred while getting translation for [${string}] in [${this.language}], Translation for ${string} does not exists.`);
